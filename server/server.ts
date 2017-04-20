ad3d820516854167b4a5ceaf51ef72d2bf0ef9cd
Will保哥 <doggy.huang@gmail.com>
2017-04-20 14:59:28 +0800

    Rename JS to TS

--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,15 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser');
+
 exports.app = express();
+
 exports.app.use(express.static(__dirname + '/../public'));
 exports.app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 var server = http.createServer(exports.app);
 server.listen(PORT);
 console.log("RUNNING " + PORT);
+
